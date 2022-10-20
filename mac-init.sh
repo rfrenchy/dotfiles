fce83328ed6ca4008252a2b48e01b993d8dbf077
Ryan French <ryanfrench@outlook.com>
2022-10-20 10:10:41 +0100

    Update mac-init.sh
    vim - nerd tree

--- mac-init.sh
+++ mac-init.sh
@@ -2,7 +2,11 @@
 
 brew install neovim
 brew install go
+brew install git
 
-# Vim Plugin Manager
+# Vim - Plugin Manager
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Vim - Nerd Tree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
