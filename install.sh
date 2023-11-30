4564a656a8baf0d7e0af976eeb0795b60aeff67b
Ryan French <ryanfrench@outlook.com>
2023-11-30 17:47:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
--- brew
--- brew install neovim
--- brew install gotags
-brew install bat
-brew install tmux
+mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
-brew install virtualenv -- depency of: https://github.com/ms-jpq/chadtree
+brew install bat -- fancy cat
+brew install bottom -- fancy sys info
+brew install git
+brew install gotags
+
+brew install jq -- json helper
+brew install lolcat 
+brew install openvpn
+brew install pipx -- python package management 
+
+brew install postgresql@16
+brew install python@3.11
+brew install tmux -- mUlTiPlExEr
+brew install virtualenv -- depency of: https://github.com/ms-jpq/chadtree (nvim plugin)
