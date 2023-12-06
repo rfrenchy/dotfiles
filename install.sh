5cd31bd22f21b5a278e3c4dcb1ec1d13fcc7a750
Ryan French <ryanfrench@outlook.com>
2023-12-06 21:51:34 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
 
 brew install bat -- fancy cat
-brew install bottom -- fancy sys info
+brew install bottom -- fancy system monitor
 brew install git
 brew install gotags
 
@@ -16,6 +16,7 @@ brew install python@3.11
 brew install virtualenv -- depency of: https://github.com/ms-jpq/chadtree (nvim plugin)
 
 brew install eza -- modern ls
+brew install inotify-tools
 
 brew install tmux -- mUlTiPlExEr
 brew install tpm -- tmux plugin manager
